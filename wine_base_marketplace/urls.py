fd03821660c7b4675025a77aa83f8e6c36aa3870
Sebastian <sebastianzco05@gmail.com>
2023-11-06 11:30:41 -0500

    Las URLS de login ya fueron arregladas

--- wine_base_marketplace/urls.py
+++ wine_base_marketplace/urls.py
@@ -3,5 +3,6 @@ from wine_base_marketplace import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('login/',views.login, name="login"),
     path('signup/',views.signup,name='signup')
 ]  
